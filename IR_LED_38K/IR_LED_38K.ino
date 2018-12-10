d51fe5108cda9cbc57d23f9270fb42dd98c1ec3d
helioz2000 <helioz2000@users.noreply.github.com>
2018-12-11 09:04:21 +1100

    Use timer interrupt

--- IR_LED_38K/IR_LED_38K.ino
+++ IR_LED_38K/IR_LED_38K.ino
@@ -3,16 +3,27 @@
  * 
  */
 
-#define LED_PIN 0
+//Attiny85 , running @ 8MHZ
+// Using timer 1
+//
+//                           +-\/-+
+//  Ain0       (D  5)  PB5  1|    |8   VCC
+//  Ain3       (D  3)  PB3  2|    |7   PB2  (D  2)  INT0  Ain1
+//  Ain2       (D  4)  PB4  3|    |6   PB1  (D  1)        pwm1
+//                     GND  4|    |5   PB0  (D  0)        pwm0
+//                           +----+
 
-void setup() { 
-  pinMode(LED_PIN, OUTPUT);
+void setup() {
+  pinMode(1, OUTPUT);
+  TCNT1 = 0;
+  TCCR1 = 0;
+  GTCCR |= (1 << PSR1); //section 13.3.2 reset the prescaler
+  TCCR1 |= (1 << CTC1); // section 12.3.1 CTC mode
+  TCCR1 |= (1 << COM1A0); //togle pin PB1 table 12-4
+  TCCR1 |= (1 << CS10); //prescaler 1 table 12-5
+  OCR1C = 104;
+  OCR1A = 104;
 }
 
-// the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_PIN, HIGH); 
-  delayMicroseconds(13);
-  digitalWrite(LED_PIN, LOW);    
-  delayMicroseconds(13);
 }
