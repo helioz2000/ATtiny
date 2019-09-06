e3ec8e022737cb381b2442e1389ffd3482d98dff
Erwin Bejsta <helioz2000@gmail.com>
2019-09-06 19:31:08 +1000

    Update Blink_Tiny.ino

--- Blink_Tiny/Blink_Tiny.ino
+++ Blink_Tiny/Blink_Tiny.ino
@@ -13,8 +13,15 @@
   by Scott Fitzgerald
  */
 
-#define LED_PIN 0       //0 on Digispark, 1 on DIL socket digispark compatible, 13 on Nano 
+//
+//                           +-\/-+
+//  Ain0       (D  5)  PB5  1|    |8   VCC
+//  Ain3       (D  3)  PB3  2|    |7   PB2  (D  2)  INT0  Ain1
+//  Ain2       (D  4)  PB4  3|    |6   PB1  (D  1)        pwm1
+//                     GND  4|    |5   PB0  (D  0)        pwm0
+//                           +----+
 
+#define LED_PIN PB1   //0 on Digispark, 1 on DIL socket digispark compatible, 13 on Nano 
 
 // the setup function runs once when you press reset or power the board
 void setup() {
